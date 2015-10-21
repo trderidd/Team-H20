5a2c1e0e7fca3d91e99235bce525f48768f5b24e
trderidd <teddyd92@gmail.com>
2015-10-21 17:44:18 -0400

    Working Demo Submission
    The build  used for the First Demo Submission
    Includes:
    *Condenser
    *Evaporator
    *Large Terrain
    *Finish Counter Box

---
+++ Working Demo/Assets/Custom Scripts/CountBox.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System;
+
+public class CountBox : MonoBehaviour
+{
+    int particlesFinished = 0;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("entered box");
+
+        particlesFinished++;
+
+        if (particlesFinished >= 100)
+        {
+            Debug.Log("completion");//level complete message
+            Application.LoadLevel("level2"); //can use index later when we have more levels
+        }
+
+        //Destroy(col.gameObject);
+
+    }
+
+    void OnTriggerExit2D(Collider2D thing)
+    {
+        Debug.Log("exited box");
+
+
+    }
+	private GUIStyle guiStyle = new GUIStyle();
+	void OnGUI()
+	{
+		guiStyle.fontSize =50;
+		GUILayout.Label(particlesFinished.ToString() + "/100% Complete", guiStyle);
+	}
+
+
+}
